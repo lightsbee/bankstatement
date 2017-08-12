fd2e5e1c0e8c4124f24025a6a5196e5f5a991ab2
Anh Duc <zephyr.ark@gmail.com>
2017-08-12 22:04:31 +0700

    last day

--- dest/public/js/action.js
+++ dest/public/js/action.js
@@ -1,2 +1,3 @@
 
-'use strict';var myDropzone=new Dropzone("div#my-awesome-dropzone",{url:"/file/post"});
+'use strict';var list=document.getElementById('progress'),next=document.getElementById('next'),clear=document.getElementById('clear'),children=list.children,completed=0;next.addEventListener('click',function(){completed=(completed===0)?1:completed+2;if(completed>children.length)return;for(var i=0;i<completed;i++){children[i].children[0].classList.remove('grey');children[i].children[0].classList.add('green');if(i%2===0){children[i].children[0].classList.add('activated');}}},false);clear.addEventListener('click',function(){for(var i=0;i<children.length;i++){children[i].children[0].classList.remove('green');children[i].children[0].classList.remove('activated');children[i].children[0].classList.add('grey');}
+completed=0;},false);
